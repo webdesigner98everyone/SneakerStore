e66a7dccb1e0c24c36f2943ee112c3afb9ab88dd
webdesigner98everyone <luisfoguz1198@gmail.com>
2024-06-09 13:49:53 -0500

    Actualizacion Terminos y condiciones

--- Js/Terminos.js
+++ Js/Terminos.js
@@ -1,17 +1,24 @@
-function aceptarTerminos() {
-    // Ocultar completamente el modal de términos y condiciones
-    var modal = document.getElementById('modal-terminos');
-    modal.style.display = 'none';
-    modal.classList.remove('show'); // Eliminar la clase 'show' del modal
-    modal.style.height = 'auto'; // Restablecer la altura a auto
-}
-
-function rechazarTerminos() {
-    // Cerrar el modal de términos y condiciones
-    // document.getElementById("modal-terminos").style.display = "none";
-    alert("Debe aceptar los términos y condiciones para continuar utilizando el sitio.");
-}
-
 window.onload = function() {
-    document.getElementById('modal-terminos').style.display = 'block'; // Mostrar el modal de términos
+    var modal = document.getElementById('modal-terminos');
+    var aceptoTerminos = localStorage.getItem('aceptoTerminos');
+
+    // Verificar si el usuario ya aceptó los términos previamente
+    if (!aceptoTerminos) {
+        modal.style.display = 'block'; // Mostrar el modal de términos
+    }
+
+    // Función para aceptar los términos
+    function aceptarTerminos() {
+        modal.style.display = 'none'; // Ocultar el modal de términos
+        localStorage.setItem('aceptoTerminos', true); // Almacenar en el almacenamiento local que el usuario aceptó los términos
+    }
+
+    // Función para rechazar los términos
+    function rechazarTerminos() {
+        alert("Debe aceptar los términos y condiciones para continuar utilizando el sitio.");
+    }
+
+    // Asignar las funciones a los botones de aceptar y rechazar
+    document.getElementById('btn-aceptar-terminos').addEventListener('click', aceptarTerminos);
+    document.getElementById('btn-rechazar-terminos').addEventListener('click', rechazarTerminos);
 };
